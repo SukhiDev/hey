70db3b68f8054f2030c6d440759f3a2610d10a87
SukhiDev <100707042+SukhiDev@users.noreply.github.com>
2022-03-10 20:27:58 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/ExampleApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class ExampleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure( SpringApplicationBuilder app){
+		return app.sources(ExampleApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
